dd6cdc3527f8dfcf3000ae03bae11ca0c54ee393
gandib <gandibroy11@gmail.com>
2024-07-09 16:57:39 +0600

    Initial setup completed

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="sm:container px-1">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
