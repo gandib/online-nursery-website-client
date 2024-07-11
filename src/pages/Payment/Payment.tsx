b3dc8b46935efb76a3d6ced5201bf96d787a330a
gandib <gandibroy11@gmail.com>
2024-07-12 02:03:21 +0600

    payment page completed

--- src/pages/Payment/Payment.tsx
+++ src/pages/Payment/Payment.tsx
@@ -1,10 +1,60 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
 const Payment = () => {
+  const [select, setSelect] = useState(false);
+  const [paymentType, setPaymentType] = useState("");
+  const navigate = useNavigate();
+
+  const handlePayment = () => {
+    if (paymentType === "cod") {
+      toast("Your selected Cash on Delivery!");
+    } else {
+      toast("Your payment successful!");
+    }
+    navigate("/");
+  };
+
   return (
-    <div>
-      <h1>Payment Type</h1>
-      <div>
-        <p>Online</p>
-        <p>Cash On Delivery (COD)</p>
+    <div className="my-8">
+      <h1 className="flex justify-center text-3xl font-bold text-[#111111] mb-6">
+        Select Payment Type
+      </h1>
+      <div className="flex justify-center">
+        <div>
+          <div className="flex justify-start items-center">
+            <input
+              onClick={() => {
+                setSelect(!select);
+                setPaymentType("online");
+              }}
+              type="checkbox"
+              className="checkbox checkbox-primary"
+            />
+            <p className="p-2">Online</p>
+          </div>
+          <div className="flex justify-start items-center">
+            <input
+              onClick={() => {
+                setSelect(!select);
+                setPaymentType("cod");
+              }}
+              type="checkbox"
+              className="checkbox checkbox-primary"
+            />
+            <p className="p-2">Cash On Delivery (COD)</p>
+          </div>
+          <div className="mt-4">
+            <button
+              onClick={handlePayment}
+              disabled={!select && true}
+              className="btn bg-[#0080ff] text-white text-xl font-bold"
+            >
+              Payment
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
